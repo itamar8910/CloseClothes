52ac972a2492af2cbd84b025358467110058a961
itamar8910 <itamar8910@gmail.com>
2017-11-10 12:24:59 +0200

    NN is asymmetrical, the bug was in extract_feats

--- closest_feat.py
+++ closest_feat.py
@@ -5,7 +5,7 @@ import os
 def closest_feat(index,names_feats):
     return min([(name,np.linalg.norm(names_feats[index][1] - feats)) for name,feats in names_feats if name != names_feats[index][0]]
 ,key=lambda x:x[1])
-#TODO nearest_neighbor not symettrical => bugged
+
 def closest_pairs(dir_path):
     """returns a list of tuples (feature,neighbor,distance)"""
     names_feats = [(f,np.load(os.path.join(dir_path,f)).flatten()) for f in os.listdir(dir_path)]
