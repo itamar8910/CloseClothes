00db6d5026810c47141239114e83763d24c12c14
Itamar Shenhar <itamar8910@gmail.com>
2017-11-14 12:49:27 +0200

    added KNN for a specific sample & altered cli for closest_feats.py

--- closest_feat.py
+++ closest_feat.py
@@ -15,8 +15,27 @@ def closest_pairs(dir_path):
         pairs.append((names_feats[index][0],neighbor_name,distance))
     return sorted(pairs, key=lambda x: int(x[0][:x[0].index(".npy")]))
 
+def get_KNN(feats_dir, sample_name, k = 1):
+    sample_feats = np.load(os.path.join(feats_dir, sample_name+".npy")).flatten()
+    names_feats = [(name.replace(".npy",''),np.load(os.path.join(feats_dir,name)).flatten()) for name in os.listdir(feats_dir) if name.replace(".npy",'') != sample_name]
+
+    return [x[0] for x in sorted(names_feats, key=lambda x : np.linalg.norm(sample_feats - x[1]))[:k]]  
+
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
+    # closest_feat.py feats_path [mode=--pairs/--knn]
+    if len(sys.argv) < 3:
+        print("Usage: closest_feat.py feats_path [mode=--pairs/--knn sample_name k]")
+        exit()
+    if sys.argv[2] == '--pairs':
         print(closest_pairs(sys.argv[1]))
-    else:
-        print("Missing path to features")
+    elif sys.argv[2] == '--knn':
+        if len(sys.argv) < 4:
+            print("need to give sample_name for KNN")
+            exit()
+        if len(sys.argv) < 5:
+            k = 1
+            print("missing value for k, defaulting to K=1")
+        else:
+            k = int(sys.argv[4])
+        print(get_KNN(sys.argv[1], sys.argv[3], k=k))
+
