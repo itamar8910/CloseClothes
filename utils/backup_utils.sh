949bdd0777a3b7134ed21f89a810d945c90df376
Tadaboody <tomerpet@gmail.com>
2017-12-12 13:18:44 +0200

    Changed to custom keras fork
    fork allows us to use ImageDataGenerator with regression by adding a custom y_output-callback

--- utils/backup_utils.sh
+++ utils/backup_utils.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-#>utils.txt #clear file
+>utils.txt #clear file
 current_repo=$(git config --get remote.origin.url);
 for dir in ./*/
 do
 	cd $dir;
 	util_repo=$(git config --get remote.origin.url);
 	[ "$util_repo" != "$current_repo" ] &&
-		git config --get remote.origin.url >>../utils.txt 
+		$utils_repo >>../utils.txt 
 	cd ..;
 done
