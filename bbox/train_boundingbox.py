5d65a4f55062013efc2c404afb3e81dd6f44bd96
Tadaboody <tomerpet@gmail.com>
2017-12-13 23:19:36 +0200

    fix merge pains

--- bbox/train_boundingbox.py
+++ bbox/train_boundingbox.py
@@ -8,6 +8,7 @@ from keras.layers import Conv2D, MaxPooling2D
 from keras.optimizers import SGD
 import json
 import cv2
+from os import listdir
 from os.path import join, dirname, realpath
 from PIL import Image, ImageDraw
 import time
@@ -40,6 +41,7 @@ def gen_XY_data(bbox_json_path, input_shape, rel_img_path, MAX_SAMPLES = None,ve
            # print('time left:{0:.1f} s'.format(time_left))
             print('progress: {0:.1f}%, time left: {1:.1f}s'.format(index/float(min(len(bbox_json), MAX_SAMPLES))*100.0, time_left))
         #print(join(rel_img_path, img_data['image_path']))
+        print("reading image from path:" , join(rel_img_path, img_data['image_path']))
         img = cv2.imread(join(rel_img_path, img_data['image_path']), 0)
         #print img.shape
         orig_shape = img.shape
@@ -74,13 +76,25 @@ def gen_XY_data(bbox_json_path, input_shape, rel_img_path, MAX_SAMPLES = None,ve
 
     return np.array(X), np.array(Y)
 
+def save_model(model, base_name = "a_model", models_save_path = "utils/saved_models"):
+    # todo: test this function
+    # saved models are named 'base_name_<index>'
+    # todo: check if model_save_path is a directory, if not create it
+    same_base_names = [x for x in listdir(models_save_path) if base_name in x]
+    if len(same_base_names) == 0:   
+        max_index = -1
+    else:
+        max_index = max([int(x[x.rfind("_")+1:x.rfind(".")]) if x.rfind("_") != -1 else -1 for x in same_base_names])
+    max_index += 1
+    model.save(join(models_save_path, base_name + "_" + str(max_index) + ".h5"))
+
 if __name__ == "__main__":
     # Generate dummy data
     input_shape = (200, 200)
 
     GENERATE_DATA = True
     if GENERATE_DATA:
-        X, Y = gen_XY_data('bb_test_unresized.json', input_shape, join(dirname(realpath(__file__)), "DeepFashion/Data/"), MAX_SAMPLES = 1,verbose = True)
+        X, Y = gen_XY_data('bb_test_unresized.json', input_shape, join(dirname(realpath(__file__)), "../Data/Img/"), MAX_SAMPLES = 2,verbose = True)
         print(X.shape, Y.shape)
     X = X.reshape(*X.shape, 1)
     #Y = Y.reshape(*Y.shape, 1)
@@ -91,8 +105,8 @@ if __name__ == "__main__":
     x_train, y_train = X[:split_index], Y[:split_index]
     x_test, y_test = X[split_index:], Y[split_index:]
 
-    print(x_train.shape, y_train.shape)
-    print(x_test.shape, y_test.shape)
+    print("train x,y shapes:" , x_train.shape, y_train.shape)
+    print("test x,y shapes:", x_test.shape, y_test.shape)
 
     model = Sequential()
     # input: 100x100 images with 3 channels -> (100, 100, 3) tensors.
@@ -116,10 +130,11 @@ if __name__ == "__main__":
 
     model.compile(loss='mse', optimizer='adam')
 
-    model.fit(x_train, y_train, batch_size=32, epochs=50)
+    model.fit(x_train, y_train, batch_size=32, epochs=2)
     score_train = model.evaluate(x_train, y_train, batch_size=32)
     score_test = model.evaluate(x_test, y_test, batch_size=32)
     print(score_train)
     print(score_test)
-    model.save('save_model_{}.h5'.format(str(time.time())))
+    save_model(model, "dummy_model")
+    #model.save('save_model_{}.h5'.format(str(time.time())))
 
