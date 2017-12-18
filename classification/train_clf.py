b8e9cdffd6931a4d017a1aafeb235c1897c6b5f4
Tadaboody <tomerpet@gmail.com>
2017-12-18 21:38:20 +0200

    Add model checkpoints for classifier
    Untested

--- classification/train_clf.py
+++ classification/train_clf.py
@@ -1,3 +1,4 @@
+import os
 from resnetimpl import resnet_152_without_top
 from keras import backend as K
 from keras.models import Model
@@ -50,10 +51,16 @@ if __name__ == "__main__":
             class_mode='categorical')
 
     print("fitting")
-
+    
+    try:#Ask for forgivness, not permission
+        os.mkdir(os.join(os.path.realpth(__file__),"Checkpoints"))
+    except:
+        pass
+    filepath = os.join(os.path.realpth(__file__),"Checkpoints")
     model.fit_generator(
             train_generator,
             steps_per_epoch=2000,
             epochs=50,
             validation_data=validation_generator,
-            validation_steps=800)
+            validation_steps=800,
+            callbacks=keras.callbacks.ModelCheckpoint((filepath, monitor='val_loss', verbose=0, save_best_only=False, save_weights_only=False, mode='auto', period=1)))
