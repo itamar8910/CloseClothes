54d5ea04f38fb88a9b0e20276a13879ec882c8bb
Tadaboody <tomerpet@gmail.com>
2018-01-13 16:57:42 +0200

    Switch to using sklearn KNN class

--- feats/closest_feat.py
+++ feats/closest_feat.py
@@ -1,42 +1,40 @@
 import numpy as np
 import sys
 import os
+from sklearn.neighbors import NearestNeighbors
+from functools import partial
+import click
 
-def closest_feat(index,names_feats):
-    return min([(name,np.linalg.norm(names_feats[index][1] - feats)) for name,feats in names_feats if name != names_feats[index][0]]
-,key=lambda x:x[1])
 
-def closest_pairs(dir_path):
-    """returns a list of tuples (feature,neighbor,distance)"""
-    names_feats = [(f,np.load(os.path.join(dir_path,f)).flatten()) for f in os.listdir(dir_path)]
-    pairs = list()
-    for index in range(len(names_feats)):
-        neighbor_name,distance = closest_feat(index,names_feats)
-        pairs.append((names_feats[index][0],neighbor_name,distance))
-    return pairs
-    # return sorted(pairs, key=lambda x: int(x[0][:x[0].index(".npy")]))
+def read_single_feature_array(feat_name, feats_dir):
+    return np.load(os.path.join(feats_dir, feat_name)).flatten()
 
-def get_KNN(feats_dir, sample_name, k = 1):
-    sample_feats = np.load(os.path.join(feats_dir, sample_name+".npy")).flatten()
-    names_feats = [(name.replace(".npy",''),np.load(os.path.join(feats_dir,name)).flatten()) for name in os.listdir(feats_dir) if name.replace(".npy",'') != sample_name]
+@click.command()
+@click.argument("feats_dir")
+@click.option("--k", "-k", default=1, help="Amount of nearest neighbors for sample")
+def print_KNN(feats_dir, k):
+    debug = True
+    print(get_KNN(feats_dir, sample_name, k))
 
-    return [x[0] for x in sorted(names_feats, key=lambda x : np.linalg.norm(sample_feats - x[1]))[:k]]  
+
+
+def get_KNN(feats_dir, k):
+    """returns a list of all KNN of feature arrays in feats_dir
+    [[(FEAT1,0),(FEAT1-1nn,dist)...]]"""
+    feat_files = os.listdir(feats_dir)
+    feat_list = list(map(partial(read_single_feature_array,
+                            feats_dir=feats_dir), os.listdir(feats_dir)))
+    clf = NearestNeighbors(
+        n_neighbors=k + 1, algorithm='auto').fit(feat_list)
+    distances_matrix, indices_matrix = clf.kneighbors(feat_list)
+    ret = list()
+    for knn_indices,distances in zip(indices_matrix,distances_matrix):
+        print(knn_indices,distances)
+        index_to_file_name = map(lambda index: feat_files[index], knn_indices)
+        ret.append(tuple(zip(index_to_file_name,distances)))
+    return ret
+
 
 if __name__ == "__main__":
     # closest_feat.py feats_path [mode=--pairs/--knn]
-    if len(sys.argv) < 3:
-        print("Usage: closest_feat.py feats_path [mode=--pairs/--knn sample_name k]")
-        exit()
-    if sys.argv[2] == '--pairs':
-        print(closest_pairs(sys.argv[1]))
-    elif sys.argv[2] == '--knn':
-        if len(sys.argv) < 4:
-            print("need to give sample_name for KNN")
-            exit()
-        if len(sys.argv) < 5:
-            k = 1
-            print("missing value for k, defaulting to K=1")
-        else:
-            k = int(sys.argv[4])
-        print(get_KNN(sys.argv[1], sys.argv[3], k=k))
-
+    print_KNN()
