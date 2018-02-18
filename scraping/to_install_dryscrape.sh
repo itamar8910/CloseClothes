48ae73d1778bbc7c681c8ec14a338139d347e544
Tadaboody <tomerpet@gmail.com>
2018-02-18 12:51:57 +0200

    Change instruction file to script
    If you have a file that is two shell commands, it should be a shell script

---
+++ scraping/to_install_dryscrape.sh
@@ -0,0 +1,5 @@
+#!usr/bin/env bash
+#http://dryscrape.readthedocs.io/en/latest/installation.html
+
+sudo apt-get install qt5-default libqt5webkit5-dev build-essential python-lxml python-pip xvfb
+pip install dryscrape
