d789526beecca54066cf23d97f4a1d2c3e009b56
Tadaboody <tomerpet@gmail.com>
2018-04-10 00:30:18 +0300

    Remove dryscrape from repo
    If I see that warning one more time...

--- scraping/to_install_dryscrape.sh
+++
@@ -1,5 +0,0 @@
-#!usr/bin/env bash
-#http://dryscrape.readthedocs.io/en/latest/installation.html
-
-sudo apt-get install qt5-default libqt5webkit5-dev build-essential python-lxml python-pip xvfb
-pip install dryscrape
--- scraping/utils.py
+++ scraping/utils.py
@@ -1,8 +1,4 @@
 import requests
-try:
-    import dryscrape
-except ImportError:
-    print("Dryscrape not installed") #for work on windows
 import time
 from bs4 import BeautifulSoup
 import os
@@ -11,14 +7,6 @@ from selenium import webdriver
 import warnings
 def get_html(url):
     return (requests.get(url).text)
-
-def get_html_dryscrape(url):
-    t1 = time.time()
-    session = dryscrape.Session()
-    session.set_attribute('auto_load_images', False)
-    session.visit(url)
-    print(time.time() - t1)
-    return session.body()
 
 def get_html_selenium(url, verbose = False):
     t1 = time.time()
