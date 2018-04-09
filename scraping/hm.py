95ef5be2afb911637f708aceec0d464627a04390
Itamar Shenhar <itamar8910@gmail.com>
2018-04-09 20:53:10 +0300

    collect categories & filter irrelevant items

--- scraping/hm.py
+++ scraping/hm.py
@@ -6,8 +6,7 @@ import dryscrape
 
 from ScrapeProduct import ScrapeProduct
 from utils import get_html_selenium, get_html
-from multiprocessing.dummy import Pool  # This is a thread-based Pool
-from multiprocessing import cpu_count
+
 import pickle
 from utils import get_html_selenium
 
@@ -54,16 +53,22 @@ def get_all_color_variants_url_of_product(prod_soup, prod_url):
     return [prod_base_url + color_prod.find('a')['href'] for color_prod in prod_soup.find('ul', attrs={'id':'options-articles'}).findAll('li')]
 
 
-def scrape_castro_category(html, gender, category_url="N/A"):
+def irrelevant_product(prod : HMProduct) -> bool:
+    bad_names = ["trouses", "socks", "thong", "pants"]
+    return any([x in prod.name.lower() for x in bad_names])
+
+
+def scrape_hm_category(html, gender, category_url="N/A"):
     global N_PRODUCTS, N_SCRAPED
     print("starting category:", category_url)
     soup = BeautifulSoup(html, 'html.parser')
     data = soup.findAll('a', attrs={'class': 'product-url js-product-tracking js-product-tracking-initialized'})
     products = []
     for prod_box in data:
+        print(prod_box['href'])
         prod_soup = BeautifulSoup(get_html_selenium(prod_box['href']), 'html.parser')
         prod_colors_urls = get_all_color_variants_url_of_product(prod_soup, prod_box['href'])
-        print(prod_colors_urls)
+        print("color variants:", prod_colors_urls)
         for prod_color_url in prod_colors_urls:
             products.append(
                 {'url': prod_color_url,
@@ -78,59 +83,31 @@ def scrape_castro_category(html, gender, category_url="N/A"):
         prod_soup = prod['soup'] if prod['soup'] else BeautifulSoup(get_html_selenium(prod['url']), 'html.parser')
         N_SCRAPED += 1
         prod = HMProduct(prod['href'], gender, soup=prod_soup)
-        products.append(prod)
+        if not irrelevant_product(prod):
+            products.append(prod)
 
     print("Finished category:", category_url)
     return products
-#
-#
-# def scrape_castro(save_path):
-#     categories = {
-#         'Male': [
-#             'https://www.castro.com/he/MEN/T-shirts.html',
-#             'https://www.castro.com/he/MEN/Jumpers.html',
-#             'https://www.castro.com/he/MEN/Knits.html',
-#             'https://www.castro.com/he/MEN/Coats.html',
-#             'https://www.castro.com/he/MEN/Shirts.html',
-#             'https://www.castro.com/he/MEN/Polo-Shirts.html',
-#             'https://www.castro.com/he/catalog/category/view/id/1820',
-#             'https://www.castro.com/he/MEN/Tank-tops.html',
-#         ],
-#         'Female': [
-#             'https://www.castro.com/he/WOMEN/Dresses.html',
-#             'https://www.castro.com/he/WOMEN/Tops.html',
-#             'https://www.castro.com/he/WOMEN/Knits.html',
-#             'https://www.castro.com/he/WOMEN/Jumpers.html',
-#             'https://www.castro.com/he/WOMEN/Blazers.html',
-#             'https://www.castro.com/he/WOMEN/Coats.html',
-#             'https://www.castro.com/he/WOMEN/Jumpsuits.html',
-#         ]
-#     }
-#     products = []
-#     N_THREADS = 8
-#     pool = Pool(N_THREADS)
-#     results = pool.starmap(scrape_castro_category,
-#                            [(get_html(category_url), gender, category_url) for gender in categories.keys() for
-#                             category_url in categories[gender]])
-#     print(results)
-#     scrapes = []
-#     for res in results:
-#         scrapes.extend(res)
-#     # with open('castro_save.p', 'wb') as f:
-#     #     pickle.dump(scrapes, f)
-#     ScrapeProduct.json_from_scrapes(save_path, scrapes)
 
+HM_CATEGORIES = {
+        'Male': [
+            "http://www.hm.com/il/products/men/jumpers_cardigans",
+            "http://www.hm.com/il/products/men/outerwear",
+            "http://www.hm.com/il/products/men/shirts",
+            "http://www.hm.com/il/products/men/tshirt",
+            "http://www.hm.com/il/products/men/blazers_suits",
+            "http://www.hm.com/il/products/men/hoodies_sweatshirt"
+        ],
+        'Female': [
+            'http://www.hm.com/il/products/ladies/basics/tops',
+            'http://www.hm.com/il/products/ladies/basics/cardigansjumpers',
+            'http://www.hm.com/il/products/ladies/basics/dresses_skirts',
+            'http://www.hm.com/il/products/ladies/blazers',
+            'http://www.hm.com/il/products/ladies/tops',
+            'http://www.hm.com/il/products/ladies/cardigans_jumpers',
+            'http://www.hm.com/il/products/ladies/hoodies_sweatshirts',
+            'http://www.hm.com/il/products/ladies/dresses',
+            'http://www.hm.com/il/products/ladies/jumpsuits',
+        ]
+    }
 
-if __name__ == "__main__":
-    print(HMProduct('http://www.hm.com/il/product/94016?article=94016-M', 'male'))
-    exit()
-    # url = 'http://www.hm.com/il/product/92328?article=92328-F'
-    # soup = BeautifulSoup(get_html_selenium(url), 'html.parser')
-    # print(get_all_color_variants_url_of_product(soup, url))
-    # exit()
-    category_url = 'http://www.hm.com/il/products/men/tshirt'
-    # category_url += '?page=100' # to auto-load all items
-    scrape_castro_category(get_html_selenium(category_url), 'male', category_url)
-    # print(HMProduct('http://www.hm.com/il/product/83134?article=83134-A', 'male'))
-    #print(HMProduct('http://www.hm.com/il/product/96562?article=96562-E', 'male'))
-    # print(HMProduct('http://www.hm.com/il/product/00566?article=00566-B', 'male'))
