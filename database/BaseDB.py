7db214239a143b6946309776f9be7d33f5920a48
Tomer Keren <tomer.keren.dev@gmail.com>
2018-06-06 18:51:44 +0300

    Knn handles multiple images

--- database/BaseDB.py
+++ database/BaseDB.py
@@ -2,7 +2,7 @@ from typing import Dict, List
 import json
 import numpy as np
 import abc
-from algorithm.feats.FeatsExtractor import FeatsExtractor
+from algorithm.feats import FeatsExtractor
 from tqdm import tqdm
 from sklearn.neighbors import NearestNeighbors
 import pickle
@@ -12,8 +12,8 @@ class BaseDB(abc.ABC):
     def __init__(self, path,knn_path=KNN_PATH):
         self._path = path
         try:        
-            with open(knn_path, 'rb') as f:
-                self.__knn_clasifier = pickle.load(f)
+            with open(knn_path, 'rb') as fp:
+                self.__knn_clasifier = pickle.load(fp)
         except FileNotFoundError:
             self.__knn_clasifier = None
 
@@ -21,30 +21,32 @@ class BaseDB(abc.ABC):
 
     @abc.abstractmethod
     def add_item(self, url : str, item : Dict) -> Dict:
-        ...
+        pass
         
     @abc.abstractmethod
     def remove_item(self, url : str):
-        ...
+        pass
 
     @abc.abstractmethod
     def get_by_url(self, url : str) -> Dict:
-        ...
+        pass
     
     @abc.abstractmethod
     def update_feats(self, url : str, feats: List[np.ndarray]):
-        ...
+        pass
     
     @property
     @abc.abstractmethod
     def feat_extractor(self) -> FeatsExtractor:
-        ...
+        pass
     
     def init_knn(self, save_path = KNN_PATH):
         clf = NearestNeighbors()
         # TODO: run fit every time you the db changes
         all_items = self.get_all()
-        clf.fit([feats for item in all_items for feats in item['feats']],[item['url'] for item in all_items for feat in item['feats']]) # flattened array https://stackoverflow.com/a/952952/4342751
+        X = np.stack([np.array(feats) for item in all_items for feats in item['feats']])
+        labels = [item['url'] for item in all_items for feat in item['feats']]
+        clf.fit(X,labels) # flattened array https://stackoverflow.com/a/952952/4342751
         if save_path:
             with open(save_path, 'wb') as f:
                 pickle.dump(clf, f)
@@ -76,15 +78,16 @@ class BaseDB(abc.ABC):
                 self.update_feats(item['url'],item_feats)
 
     def get_all(self):
-        ...
+        pass
 
     @classmethod
     def init_from_json(cls, db_path, *json_paths):
         db = cls(db_path)
         for json_path in json_paths:
             print(json_path)
-            with open(json_path, 'r') as f:
-                items = json.load(f)
+            with open(json_path, 'r') as fp:
+                items = json.load(fp)
                 for item in items:
                     db.add_item(url=item['url'], item=item)
         return db
+
